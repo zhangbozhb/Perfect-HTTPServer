ad2c6ebf5b500fcc4d9c4ae3cde2094c9f389d5e
Florian Kostenzer <florian.kostenzer@icloud.com>
2020-08-02 11:46:08 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,19 @@
-// swift-tools-version:4.1
-// Generated automatically by Perfect Assistant 2
-// Date: 2018-03-28 18:54:02 +0000
+// swift-tools-version:5.2
+
 import PackageDescription
 
 let package = Package(
 	name: "PerfectHTTPServer",
+    platforms: [
+        .macOS(.v10_15)
+    ],
 	products: [
 		.library(name: "PerfectHTTPServer", targets: ["PerfectHTTPServer"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", from: "3.1.2"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.12"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-CZlib-src.git", from: "0.0.0")
+        .package(url: "https://github.com/123FLO321/Perfect-Net.git", .branch("swift5")),
+		.package(url: "https://github.com/123FLO321/Perfect-HTTP.git", .branch("swift5")),
+		.package(url: "https://github.com/123FLO321/Perfect-CZlib-src.git", .branch("swift5"))
 	],
 	targets: [
 		.target(name: "PerfectCHTTPParser", dependencies: []),
